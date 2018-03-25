12b10be98adf759ce1d6d135aaf3e3d604b3a3f0
Hazem Khaled <hazem.khaled@gmail.com>
2018-03-25 21:51:00 +0300

    implement /search in backend return dummy results

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,27 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
+const url = require('url');
 
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+const app = new Koa();
+const router = new Router();
+
+router.get('/search', (ctx, next) => {
+    ctx.is('application/json'); // => 'application/json'
+
+    ctx.body = [
+        { sku: '123', name: 'Product #1' },
+        { sku: '234', name: 'Product #2' },
+        { sku: '345', name: 'Product #3' },
+        { sku: '456', name: 'Product #4' }
+    ];
+
+    const query = url.parse(ctx.req.url, true).query;
+
+    console.log(query.keyword);
 });
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
 
 app.listen(8080);
