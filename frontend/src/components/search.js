713e6aba7c542e694a1d28cbe899714ae14a67c7
Hazem Khaled <hazem.khaled@gmail.com>
2018-03-25 21:51:36 +0300

    Send fetch request from frontend to backend asking for search result

--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -1,35 +1,58 @@
 import React, { Component } from 'react';
 import searchIcon from '../images/search.png';
-import { Glyphicon } from 'react-bootstrap';
+import { Glyphicon, Row, ListGroup, ListGroupItem } from 'react-bootstrap';
 import './search.css';
 
 class Search extends Component {
+    state = { products: [], searchResultClass: 'hide' };
 
-    constructor(props) {
-        super(props);
-        this.state = { searchResultClass: 'hide' };
-    }
-
-    search(event) {
+    handleSearchChange = (event) => {
         const value = event.target.value.trim();
 
-        this.setState({ comment: 'Hello' });
-
         this.setState({
+            products: [],
             searchResultClass: value.length > 2 ? '' : 'hide'
         });
 
+        if (value.length <= 2) {
+            return;
+        }
+
         console.log('Search for', value);
         //TODO: Fetch from API
+        fetch('/search?keyword=' + encodeURIComponent(value))
+            .then((response) => {
+                return response.json();
+            })
+            .then((products) => {
+                console.log(products);
+                this.setState({
+                    products,
+                    searchResultClass: value.length > 2 ? '' : 'hide'
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     render() {
+        const { products } = this.state;
+
+        let productRows = products.map((product) => (
+            <ListGroupItem key={product.sku}>{product.name}</ListGroupItem>
+        ));
+
+        productRows = productRows || <Glyphicon glyph="refresh" />;
+
         return (
             <div className="search-container">
-                <input placeholder="Hello, I'm looking for..." onKeyUp={this.search.bind(this)} />
+                <input placeholder="Hello, I'm looking for..." onKeyUp={this.handleSearchChange} />
                 <img src={searchIcon} alt="search" />
                 <div className={`search-result ${this.state.searchResultClass}`}>
-                    <Glyphicon glyph="refresh" />
+                    <ListGroup>
+                        {productRows}
+                    </ListGroup>
                 </div>
             </div>
         );
