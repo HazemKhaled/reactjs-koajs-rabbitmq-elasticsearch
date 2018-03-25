614ae1ce17b98cbff38e32c642b17b2f19c08cc4
Hazem Khaled <hazem.khaled@gmail.com>
2018-03-25 19:38:06 +0300

    Search state and loading spinner

--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import searchIcon from '../images/search.png';
+import { Glyphicon } from 'react-bootstrap';
 import './search.css';
 
 class Search extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { searchResultClass: 'hide' };
+    }
+
+    search(event) {
+        const value = event.target.value.trim();
+
+        this.setState({ comment: 'Hello' });
+
+        this.setState({
+            searchResultClass: value.length > 2 ? '' : 'hide'
+        });
+
+        console.log('Search for', value);
+        //TODO: Fetch from API
+    }
+
     render() {
         return (
             <div className="search-container">
-                <input placeholder="Hello, I'm looking for..." />
+                <input placeholder="Hello, I'm looking for..." onKeyUp={this.search.bind(this)} />
                 <img src={searchIcon} alt="search" />
+                <div className={`search-result ${this.state.searchResultClass}`}>
+                    <Glyphicon glyph="refresh" />
+                </div>
             </div>
         );
     }
